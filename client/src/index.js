8e0981d5c8a585766d91d9e440dc1c82fdfeab51
Nicolas Burgess <nburgessz@gmail.com>
2018-06-19 19:57:44 -0400

    heroku-postbuild

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
+import App from './App/App';
 
 render((
     <BrowserRouter>
